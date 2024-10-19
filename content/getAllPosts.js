0afaec4174bd5f64097b45faba8a0011b866774e
lebao3105 <bao12345yocoo@gmail.com>
2024-10-19 18:36:14 +0700

    Hot fixes

--- content/getAllPosts.js
+++ content/getAllPosts.js
@@ -1,84 +1,39 @@
-function getCardCSS()
+async function loadPosts(path)
 {
-    for (let i = 0; i < document.styleSheets.length; i++)
+    const response = await fetch(
+        'https://api.github.com/repos/lebao3105/lebao3105.github.io/contents/content/' + path)
+    const data = await response.json()
+    const targetList = document.getElementsByClassName("grid")[0];
+
+    for (let file of data)
     {
-        const sheet = document.styleSheets[i];
+        var newElement = document.createElement('div')
+        const titleAndTime = getHTMLTitleAndLastModified(path + '/' + file.name)
+        newElement.innerHTML = titleAndTime[0] + "<br><br>" + titleAndTime[1]
 
-        try {
-            const rules = sheet.cssRules || sheet.rules; // for cross browser compability
-            for (let j = 0 ; j < rules.length; j++)
-            {
-                const rule = rules[j];
-                if (rule.selectorText === '.card' && rule instanceof CSSStyleRule)
-                {
-                    return rule.style;
-                }
-            }
-        }
-        catch (e)
-        {
-            // do something
-        }
+        newElement.style.backgroundColor = "lightgray"
+        newElement.style.paddingLeft = "10px"
+        newElement.style.cursor = "pointer"
+
+        targetList.appendChild(newElement)
     }
 }
 
-function loadPosts(path)
+function getHTMLTitleAndLastModified(path)
 {
     var xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200)
-        {
-            parseAndReturnPosts(this, path);
-        }
-    }
-    xmlHttp.open('GET', '/content/' + path, false);
-    xmlHttp.send(null);
-}
-
-function getHTMLTitle(path)
-{
-    var xmlHttp = new XMLHttpRequest();
-    var result = '';
+    var result = [];
 
     xmlHttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200)
         {
             var htmlDoc = new DOMParser().parseFromString(this.responseText, 'text/html')
-            result = htmlDoc.getElementsByTagName('title')[0].innerText;
+            result.push(htmlDoc.getElementsByTagName('title')[0].innerText);
+            result.push(htmlDoc.getElementById('last-updated').innerText);
         }
     }
     xmlHttp.open('GET', '/content/' + path, false);
     xmlHttp.send(null);
 
     return result;
-}
-
-function parseAndReturnPosts(xmlRequest, path)
-{
-    var parser = new DOMParser();
-    var htmlDoc = parser.parseFromString(xmlRequest.responseText, 'text/html');
-    var aList = htmlDoc.getElementsByTagName('a');
-    var targetList = document.getElementsByClassName("grid")[0];
-
-    for (i = 1; i < aList.length; i++) {
-        try {
-            var spanList = aList[i].getElementsByTagName("span");
-            if (spanList[0].innerText.endsWith('.html'))
-            {
-                var newElement = document.createElement('div');
-                newElement.innerHTML =
-                    getHTMLTitle(path + '/' + spanList[0].innerText) +
-                    "<br><br>" + spanList[2].innerText;
-
-                newElement.style.backgroundColor = "lightgray"
-                newElement.style.paddingLeft = "10px"
-                newElement.style.cursor = "pointer"
-                
-                targetList.appendChild(newElement);
-            }
-        }
-        catch (error) {
-            
-        }
-    }
 }
